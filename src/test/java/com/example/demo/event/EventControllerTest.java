30952063bdeac6e6b1c452351afeb3d3c8c9696b
copymaker <duoback0@gmail.com>
2020-02-28 21:08:40 +0900

    Set Jackson fail-on-unknown-properties=true

--- src/test/java/com/example/demo/event/EventControllerTest.java
+++ src/test/java/com/example/demo/event/EventControllerTest.java
@@ -1,5 +1,11 @@
 package com.example.demo.event;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
@@ -13,13 +19,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 @SpringBootTest
 @AutoConfigureMockMvc
 class EventControllerTest {
@@ -32,6 +31,38 @@ class EventControllerTest {
 
     @Test
     void createEventTest() throws Exception {
+        EventDto eventDto = EventDto.builder()
+            .name("Spring")
+            .description("REST API Development with Spring")
+            .openEnrollmentDateTime(LocalDateTime.of(2020, 2, 23, 14, 21))
+            .closeEnrollmentDateTime(LocalDateTime.of(2020, 2, 24, 14, 21))
+            .beginEventDateTime(LocalDateTime.of(2020, 2, 25, 14, 21))
+            .endEventDateTime(LocalDateTime.of(2020, 2, 26, 14, 21))
+            .basePrice(100)
+            .maxPrice(200)
+            .limitOfEnrollment(100)
+            .location("강남역 D2 스타텁 팩토리")
+            .build();
+
+        mockMvc
+            .perform(
+                post("/api/events")
+                    .characterEncoding(StandardCharsets.UTF_8.name())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaTypes.HAL_JSON)
+                    .content(objectMapper.writeValueAsString(eventDto)))
+            .andDo(print())
+            .andExpect(status().isCreated())
+            .andExpect(header().exists(HttpHeaders.LOCATION))
+            .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+            .andExpect(jsonPath("id").exists())
+            .andExpect(jsonPath("id").value(Matchers.not(100L)))
+            .andExpect(jsonPath("free").value(Matchers.not(true)))
+            .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+    }
+
+    @Test
+    void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
             .id(100L)
             .name("Spring")
@@ -57,12 +88,6 @@ class EventControllerTest {
                     .accept(MediaTypes.HAL_JSON)
                     .content(objectMapper.writeValueAsString(event)))
             .andDo(print())
-            .andExpect(status().isCreated())
-            .andExpect(header().exists(HttpHeaders.LOCATION))
-            .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
-            .andExpect(jsonPath("id").exists())
-            .andExpect(jsonPath("id").value(Matchers.not(100L)))
-            .andExpect(jsonPath("free").value(Matchers.not(true)))
-            .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+            .andExpect(status().isBadRequest());
     }
 }
