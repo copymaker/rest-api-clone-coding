adf940066c87bea634c3c9ce603e8765148a315e
copymaker <duoback0@gmail.com>
2020-02-29 22:13:16 +0900

    Add Account Domain

--- src/main/java/com/example/demo/event/Event.java
+++ src/main/java/com/example/demo/event/Event.java
@@ -1,11 +1,13 @@
 package com.example.demo.event;
 
+import com.example.demo.account.Account;
 import java.time.LocalDateTime;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -26,22 +28,37 @@ public class Event {
     @Id
     @GeneratedValue
     private Long id;
+
     private String name;
+
     private String description;
+
     private LocalDateTime openEnrollmentDateTime;
+
     private LocalDateTime closeEnrollmentDateTime;
+
     private LocalDateTime beginEventDateTime;
+
     private LocalDateTime endEventDateTime;
+
     private String location;    // (optional) 이게 없으면 온라인 모임
+
     private int basePrice;      // (optional)
+
     private int maxPrice;       // (optional)
+
     private int limitOfEnrollment;
+
     private boolean offline;
+
     private boolean free;
 
     @Builder.Default
     @Enumerated(value = EnumType.STRING)
     private EventStatus eventStatus = EventStatus.DRAFT;
+
+    @ManyToOne
+    private Account manager;
 
     public void update() {
         // Update free
--- src/test/java/com/example/demo/event/EventControllerTest.java
+++ src/test/java/com/example/demo/event/EventControllerTest.java
@@ -107,6 +107,7 @@ class EventControllerTest extends BaseControllerTest {
                     fieldWithPath("free").description("it tells if this event is free or not"),
                     fieldWithPath("offline").description("it tells if this event is offline event or not"),
                     fieldWithPath("eventStatus").description("event status"),
+                    fieldWithPath("manager").description("manager"),
                     fieldWithPath("_links.self.href").description("link to self"),
                     fieldWithPath("_links.query-events.href").description("link to query event list"),
                     fieldWithPath("_links.update-event.href").description("link to update existing event"),
