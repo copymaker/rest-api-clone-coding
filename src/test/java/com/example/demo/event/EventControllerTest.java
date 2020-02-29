0e8d9dbdffddc4fc33cc295b2ad8bf89af7973b5
copymaker <duoback0@gmail.com>
2020-02-29 10:09:05 +0900

    config and test db

--- src/test/java/com/example/demo/event/EventControllerTest.java
+++ src/test/java/com/example/demo/event/EventControllerTest.java
@@ -30,12 +30,14 @@ import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @Import(RestDocsConfiguration.class)
+@ActiveProfiles("test")
 class EventControllerTest {
 
     @Autowired
